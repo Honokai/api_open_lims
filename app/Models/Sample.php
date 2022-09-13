58b12f603945e25af7109c679a545ad9318e3ff3
Honokai <emerson_hatred@hotmail.com>
2022-09-13 19:07:38 -0300

    relacionamentos

--- app/Models/Sample.php
+++ app/Models/Sample.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Sample extends Model
 {
     use HasFactory;
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\User::class);
+    }
 }
--- app/Models/SubSample.php
+++ app/Models/SubSample.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SubSample extends Model
 {
     use HasFactory;
+
+    public function sample(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\Sample::class);
+    }
 }
